84d8d3c1d557b08b78c08a123a5cccd05fb0f3ed
David N <davidcalieln@gmail.com>
2024-03-15 03:40:43 -0700

    test(tl): add add_five_works

--- tests/compiler/src/lib.rs
+++ tests/compiler/src/lib.rs
@@ -1,3 +1,10 @@
 //! Test for using `wasm2rs` as a build dependency
 
 include!(concat!(env!("OUT_DIR"), "/simple.rs"));
+
+#[test]
+fn add_five_works() {
+    let inst = wasm::Instance::instantiate(wasm::StdRuntime::default()).unwrap();
+    assert_eq!(inst.add_five(37), 42);
+    assert_eq!(inst.add_five(u32::MAX as i32), 4);
+}
