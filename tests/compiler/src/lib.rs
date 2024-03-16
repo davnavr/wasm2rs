b2e3585890a4574b6112bce585d8fe6ccac4a45b
David N <davidcalieln@gmail.com>
2024-03-15 20:30:44 -0700

    test(tl): add test for loop_factorial_unsigned

--- tests/compiler/src/lib.rs
+++ tests/compiler/src/lib.rs
@@ -17,6 +17,33 @@ fn if_else_block() {
 }
 
 #[test]
+fn factorial() {
+    let inst = wasm::Instance::instantiate(wasm::StdRuntime::default()).unwrap();
+
+    macro_rules! tests {
+        ($($input:expr => $output:expr),*) => {$(
+            assert_eq!(inst.loop_factorial_unsigned($input), Ok($output));
+        )*};
+    }
+
+    tests! {
+        0 => 1,
+        1 => 1,
+        2 => 2,
+        3 => 6,
+        4 => 24,
+        5 => 120,
+        6 => 720,
+        7 => 5040,
+        8 => 40320,
+        9 => 362880,
+        10 => 3628800,
+        11 => 39916800,
+        12 => 479001600
+    }
+}
+
+#[test]
 #[should_panic]
 fn unreachable_panic() {
     let inst = wasm::Instance::instantiate(wasm::StdRuntime::default()).unwrap();
