7d60ca9b2cad3327fb50ae694519f7d769691fcc
David N <davidcalieln@gmail.com>
2024-03-21 23:57:02 -0700

    build: make tests/compiler/build.rs use pools

--- tests/compiler/build.rs
+++ tests/compiler/build.rs
@@ -1,6 +1,12 @@
 fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
 
+    let buffer_pool = wasm2rs::buffer::Pool::default();
+    let func_validator_allocation_pool = wasm2rs::FuncValidatorAllocationPool::default();
+    let mut translation_options = wasm2rs::Translation::new();
+    translation_options.buffer_pool(&buffer_pool);
+    translation_options.func_validator_allocation_pool(&func_validator_allocation_pool);
+
     let compile_wasm = |wat: &str, name: &str| {
         let mut out_path = std::path::Path::join(out_dir.as_ref(), name);
         out_path.set_extension("rs");
@@ -15,7 +21,7 @@ fn main() {
             Err(e) => panic!("could not open output file {out_path:?}: {e}"),
         };
 
-        if let Err(e) = wasm2rs::Translation::new().translate_from_buffer(&wasm, &mut output) {
+        if let Err(e) = translation_options.translate_from_buffer(&wasm, &mut output) {
             panic!("compilation failed for {name:?}: {e}");
         }
     };
