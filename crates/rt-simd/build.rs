d840d71be8755e1a502379c46154ae261e1299a9
David N <davidcalieln@gmail.com>
2024-05-10 01:24:11 -0700

    docs(rt): hide warnings for cfg aliases

--- crates/rt-simd/build.rs
+++ crates/rt-simd/build.rs
@@ -1,6 +1,10 @@
 fn main() {
     use cfg_aliases::cfg_aliases;
 
+    println!("cargo::rustc-check-cfg=cfg(simd_intrinsics)");
+    println!("cargo::rustc-check-cfg=cfg(simd_sse2_intrinsics)");
+    println!("cargo::rustc-check-cfg=cfg(simd_no_intrinsics)");
+
     cfg_aliases! {
         simd_intrinsics: { feature = "simd-intrinsics" }, // not(miri)
         simd_sse2_intrinsics: { all(simd_intrinsics, target_feature = "sse2") },
