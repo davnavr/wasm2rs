76634d6edbfcffa9af0bc847c39f2389cdac47f2
David N <davidcalieln@gmail.com>
2024-05-31 14:05:44 -0700

    fix(rt): make nan::fnn_is_arithmetic() helpers call .is_nan()

--- crates/rt-math/src/nan.rs
+++ crates/rt-math/src/nan.rs
@@ -9,7 +9,6 @@
 //! [WebAssembly specification]: https://webassembly.github.io/spec/core/syntax/values.html#floating-point
 
 const F32_PAYLOAD_HIGH_BIT: u32 = 1u32 << 22;
-const F32_PAYLOAD_LOW_BITS: u32 = 0x003F_FFFF;
 
 /// The [*canonical NaN*] value for [`f32`]s.
 ///
@@ -34,17 +33,15 @@ pub fn is_canonical_f32(value: f32) -> bool {
 ///
 /// [*arithmetic NaN*]: https://webassembly.github.io/spec/core/syntax/values.html#floating-point
 pub fn is_arithmetic_f32(value: f32) -> bool {
-    let bits = value.to_bits();
-    value.is_nan() && (bits & F32_PAYLOAD_HIGH_BIT != 0) && (bits & F32_PAYLOAD_LOW_BITS != 0)
+    value.is_nan()
 }
 
-const PAYLOAD_HIGH_BIT_F64: u64 = 1u64 << 51;
-const PAYLOAD_LOW_BITS_F64: u64 = 0x0007_FFFF_FFFF_FFFF;
+const F64_PAYLOAD_HIGH_BIT: u64 = 1u64 << 51;
 
 /// The [*canonical NaN*] value for [`f64`]s.
 ///
 /// [*canonical NaN*]: https://webassembly.github.io/spec/core/syntax/values.html#floating-point
-pub const F64_CANONICAL: u64 = (0x07FFu64 << 52) | PAYLOAD_HIGH_BIT_F64;
+pub const F64_CANONICAL: u64 = (0x07FFu64 << 52) | F64_PAYLOAD_HIGH_BIT;
 
 /// The negative [*canonical NaN*] value for [`f64`]s.
 ///
@@ -64,6 +61,5 @@ pub fn is_canonical_f64(value: f64) -> bool {
 ///
 /// [*arithmetic NaN*]: https://webassembly.github.io/spec/core/syntax/values.html#floating-point
 pub fn is_arithmetic_f64(value: f64) -> bool {
-    let bits = value.to_bits();
-    value.is_nan() && (bits & PAYLOAD_HIGH_BIT_F64 != 0) && (bits & PAYLOAD_LOW_BITS_F64 != 0)
+    value.is_nan()
 }
