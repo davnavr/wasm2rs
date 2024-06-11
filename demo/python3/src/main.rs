0e5f58e1eab653b5307f30169bedff94041d9f63
David N <davidcalieln@gmail.com>
2024-06-10 22:35:28 -0700

    feat(py3): use Wasi+ReuseExistingMemory

--- demo/python3/src/main.rs
+++ demo/python3/src/main.rs
@@ -5,19 +5,22 @@
 //! [VMware Labs WLR]: https://github.com/vmware-labs/webassembly-language-runtimes
 
 mod embedder {
-    pub use wasm2rs_rt::embedder::self_contained::{rt, Memory0, Module, Table0};
+    pub use wasm2rs_rt::embedder::self_contained::{rt, Module, Table0};
 
     pub type Trap = wasm2rs_rt_wasip1::Trap<wasm2rs_rt::trap::TrapError>;
 
-    pub type WasiApi = wasm2rs_rt_wasip1::StdApi<Memory0, wasm2rs_rt::trap::TrapError>;
+    pub type Memory0 = std::rc::Rc<wasm2rs_rt::memory::HeapMemory>;
+
+    pub type WasiApi =
+        wasm2rs_rt_wasip1::StdApi<Memory0, wasm2rs_rt::trap::TrapError>;
 
     #[derive(Debug)]
     pub struct Imports {
-        pub wasi: WasiApi,
+        pub wasi: wasm2rs_rt_wasip1::Wasi<WasiApi>,
     }
 
     impl Imports {
-        pub fn wasi_snapshot_preview1(&self) -> &WasiApi {
+        pub fn wasi_snapshot_preview1(&self) -> &wasm2rs_rt_wasip1::Wasi<WasiApi> {
             &self.wasi
         }
     }
@@ -28,25 +31,29 @@ mod embedder {
         pub instance: wasm2rs_rt::store::AllocateModuleRc,
         pub table0:
             wasm2rs_rt::store::AllocateHeapTable<wasm2rs_rt::func_ref::FuncRef<'static, Trap>>,
-        pub memory0: wasm2rs_rt::store::AllocateHeapMemory,
+        pub memory0: wasm2rs_rt::store::ReuseExistingMemory<Memory0>,
     }
 }
 
-// TODO: rust-analyzer.files.exclude (IDE specific, sadly) or create a separate workspace
 #[rustfmt::skip]
 include!("generated/python3.wasm2.rs");
+
 wasm!(pub mod python3 use super::embedder);
 
 fn main() -> std::process::ExitCode {
-    let wasi = wasm2rs_rt_wasip1::StdApiBuilder::new()
+    let wasi_api = wasm2rs_rt_wasip1::StdApiBuilder::new()
         .inherit_standard_streams_without_sanitation()
         .build();
+
+    let memory = std::rc::Rc::new(embedder::Memory0::default());
+
+    let wasi = wasm2rs_rt_wasip1::Wasi::new(memory.clone(), wasi_api);
 
     let store = embedder::Store {
         imports: embedder::Imports { wasi },
         instance: Default::default(),
         table0: Default::default(),
-        memory0: Default::default(),
+        memory0: wasm2rs_rt::store::ReuseExistingMemory::new(memory),
     };
 
     macro_rules! return_on_trap {
